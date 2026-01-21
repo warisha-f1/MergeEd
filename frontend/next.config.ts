c6c756b6763c9b89b5c5636c4806593293f629b5
wf1 <khanwarishaaziz01@gmail.com>
2026-01-21 20:48:27 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,6 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-const nextConfig: any = { // Change 'NextConfig' to 'any' here
-  experimental: {
-    allowedDevOrigins: ['http://192.168.56.1:3000', 'http://localhost:3000'],
-  },
-};
-
-export default nextConfig;
+module.exports = nextConfig
